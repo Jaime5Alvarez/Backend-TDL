a4a554ebfd06e6606027066baab4c18519ba4d01
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2024-02-03 13:07:49 +0100

    arreglando static files

--- config/settings.py
+++ config/settings.py
@@ -147,12 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-    MEDIA_URLS ='/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Tell Django to copy statics to the `staticfiles` directory
+# in your application directory on Render.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
