60eebe182fc3baf37b2af3e5bb1a22464b174f77
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2024-02-03 20:52:22 +0100

    modify script

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 echo "BUILD START"
+
 python3.9 -m pip install -r requirements.txt
+
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
- echo "BUILD END"
+
+echo "BUILD END"
