adb7f282edaffbcdc4391b9d1b9158168a42a4ab
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2024-02-03 13:27:28 +0100

    fix build_files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+ python -m pip install -r requirements.txt
+ python manage.py collectstatic --noinput --clear
+ python manage.py makemigrations
+ python manage.py migrate
  echo "BUILD END"
