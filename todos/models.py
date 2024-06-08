87552bad7aea79f84dff1168044d313d122f63f3
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2024-06-08 17:40:36 +0200

    bugfix models

--- todos/models.py
+++ todos/models.py
@@ -9,7 +9,7 @@ class Todo(models.Model):
     date = models.DateTimeField()
     hour = models.TimeField(null=True,blank=True)
     completed = models.BooleanField(default=False)
-    user =models.ForeignKey(MyUser,on_delete=models.CASCADE   )
+    user =models.ForeignKey(MyUser,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
